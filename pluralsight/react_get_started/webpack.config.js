ec0420418cd1111af1c0d9160d3f91ba18fd3968
lemoncello <byurchak@gmail.com>
2018-01-21 17:44:53 +0200

    tutorial for pluralsight

--- pluralsight/react_get_started/webpack.config.js
+++ pluralsight/react_get_started/webpack.config.js
@@ -3,18 +3,19 @@ var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ManifestPlugin = require('webpack-manifest-plugin');
 var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 var DIST_DIR = path.resolve(__dirname, "bin");
 var scriptsDir = "static/js/";
 
 module.exports = {
   entry: './src/index.js',
-  output: { 
-    path: DIST_DIR, 
+  output: {
+    path: DIST_DIR,
     filename: scriptsDir + 'bundle.js',
     publicPath: "/bin/",
     sourceMapFilename: scriptsDir + 'bundle.map.js'
-   },
+  },
   plugins: [
     // new webpack.ProvidePlugin({
     //   $: 'jquery',
@@ -23,6 +24,7 @@ module.exports = {
     //   Popper: ['popper.js', 'default'],
     //   // In case you imported plugins individually, you must also require them here:
     // }),
+    new ExtractTextPlugin("styles.css"),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "public/index.html",
@@ -36,7 +38,45 @@ module.exports = {
   ],
   devtool: '#source-map',
   module: {
-    loaders: [
+    //   loaders: [
+    //     {
+    //       test: /.jsx?$/,
+    //       loader: 'babel-loader',
+    //       exclude: /node_modules/,
+    //       query: {
+    //         presets: ['es2015', 'react']
+    //       }
+    //     },
+    //     { 
+    //       test: /\.css$/, 
+    //       loader: "style-loader!css-loader" 
+    //     },
+    //     { 
+    //       test: /\.png$/, 
+    //       loader: "url-loader?limit=100000" 
+    //     },
+    //     { 
+    //       test: /\.jpg$/, 
+    //       loader: "file-loader" 
+    //     },
+    //     {
+    //       test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+    //       loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+    //     },
+    //     {
+    //       test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+    //       loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+    //     },
+    //     {
+    //       test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+    //       loader: 'file'
+    //     },
+    //     {
+    //       test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
+    //       loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+    //     }
+    //   ]
+    rules: [
       {
         test: /.jsx?$/,
         loader: 'babel-loader',
@@ -45,34 +85,14 @@ module.exports = {
           presets: ['es2015', 'react']
         }
       },
-      { 
-        test: /\.css$/, 
-        loader: "style-loader!css-loader" 
-      },
-      { 
-        test: /\.png$/, 
-        loader: "url-loader?limit=100000" 
-      },
-      { 
-        test: /\.jpg$/, 
-        loader: "file-loader" 
-      },
       {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url-loader?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, 
-        loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
       }
     ]
-  },
+  }
+
 };
