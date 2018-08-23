39587f2d42f2934fa391ededbae070389da213a6
byurch <byurchak@gmail.com>
2018-08-23 17:37:55 +0300

    Commit all the stuff

---
+++ pluralsight/C++/CPlus_Beginners/CPlus_Beginners/CPlusBeginners.cpp
@@ -0,0 +1,15 @@
+// CPlusBeginners.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main()
+{
+	auto x = 12;
+	std::cout << "Number is " << x << std::endl;
+	std::string name;
+    return 0;
+}
+
